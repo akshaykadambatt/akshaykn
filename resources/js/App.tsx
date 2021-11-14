e225e3d206c1c5bff227efb7f5e91d1c0bf9bc67
Akshay <akshay@spawoz.com>
2021-11-14 11:24:58 +0530

    first commit

---
+++ resources/js/App.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Home from "./components/Home";
+
+const App = () => {
+    return (
+        <Home />
+    );
+};
+
+export default App;
---
+++ resources/js/components/Home.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export interface IUser {
+    name: string;
+    age: number;
+}
+const Home = () => {
+    const [users, setUsers] = useState<IUser[]>([
+        {
+            name: "Bijaya",
+            age: 25,
+        },
+        {
+            name: "Ram",
+            age: 25,
+        },
+    ]);
+
+    return (
+        <div>
+            <h1>Users list</h1>
+            <ul>
+                {users.map((user: IUser) => {
+                    return (
+                        <li key={user.name}>
+                            {user.name} is {user.age} years old
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default Home;
