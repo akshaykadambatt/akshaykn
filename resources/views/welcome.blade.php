6beae7a063d381792693a338586db5569efa356b
Akshay K Nair <akshay@spawoz.com>
2022-01-27 15:58:50 +0530

    Loader

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,64 @@
     </style>
 </head>
 <body class="antialiased">
-    <div id="app"></div>
+    <div id="app">
+        <div class="contain-loader">
+            <p>Loading</p>
+            <div class="progress-bar-wrap"><div class="progress-bar"></div></div>
+        </div>
+    </div>
 </body>
+<style>
+    .contain-loader {
+    height: 97vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.contain-loader p {
+    font-size: 50px;
+    font-weight: lighter;
+    display: block;
+}
+
+.contain-loader .progress-bar-wrap {
+    height: 7px;
+    width: 50%;
+    border: 1px solid #000;
+}
+.contain-loader .progress-bar{
+    height: 100%;
+    width: 0%;
+    background-color: #000;
+    animation: progress 100s ease-in .3s 1 normal;
+}
+@keyframes progress {
+    0%{
+        width:0;
+    }
+    1%{
+        width:10%;
+    }
+    3%{
+        width:40%;
+    }
+    4%{
+        width:70%;
+    }
+    8%{
+        width:80%;
+    }
+    30%{
+        width:99%;
+    }
+    100%{
+        width:100%;
+    }
+}
+</style>
 <script src="{{ mix('js/manifest.js') }}" defer async></script>
 <script src="{{ mix('js/index.js') }}" defer async></script>
 <script src="{{ mix('js/vendor.js') }}" defer async></script>
